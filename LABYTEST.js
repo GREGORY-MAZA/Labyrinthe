d58a9f5d18b2e23a218a9755bf3881e9d30178f1
Greg <maza.gregory46@gmail.com>
2021-07-06 16:25:29 +0200

    BROUILLON Labytest

---
+++ LABYTEST.js
@@ -0,0 +1,107 @@
+//______________________ VARIABLES ____________________
+
+let START = 'S';
+let END = 'G';
+let wall = 'wall';
+let player = "player";
+let o = "empty";
+let exploration = 0;
+let bottom = 0;
+let right = 0;
+let up = 0;
+let left = 0;
+let checkpoint = [];
+let Y = 0;
+let X = 0;
+let possibility = 0
+
+//______________________ LABYRINTHE ____________________
+
+labyrinthe = [
+    //                [ Y ] = VERTICALE    [ X ] = HORIZONTALE
+    //       0         1        2         3         4         5         6
+    [  START,    wall,      o,        o,        o,        o,        o    ],     // #0
+    [    o,      wall,      o,       wall,     wall,      o,       wall  ],     // #1
+    [    o,      wall,      o,       wall,     END,       o,        o    ],     // #2
+    [    o,       o,        o,        o,       wall,     wall,      o    ],     // #3
+    [    o,      wall,      o,       wall,      o,        o,        o    ],     // #4
+    [    o,      wall,      o,        o,        o,       wall,      o    ]      // #5
+]
+
+while (labyrinthe[Y][X] != END){
+    Scan(Y,X)
+    Deplacement(Y,X)
+    console.table(labyrinthe)
+
+    //1ere étape - Fonction Scan pour savoir quelle case nous entoure
+    //                      Si la case Y n'est pas tout en bas du tableau ET que la case du BAS est VIDE et pas Exploré        on peut vérifier plus bas  ( +1 dans POSSIBLE )
+    //                      Si la case X n'est pas la derniere du tableau ET que la case de DROITE est VIDE et pas Exploré     on peut vérifier à droite  ( +1 dans POSSIBLE )
+    //                      Si la case Y n'est pas égale à 0 ET que la case du HAUT est VIDE et pas Exploré                    on peut vérifier en haut   ( +1 dans POSSIBLE )
+    //                      Si la case X n'est pas égale à 0 ET que la case à GAUCHE est VIDE et pas Exploré                   on peut vérifier à gauche  ( +1 dans POSSIBLE )
+    //
+    // Si POSSIBLE est plus grand ou égale à 2 alors on stock les positions Y et X dans un nouveau tableau [CHECKPOINT] et on réinitialise POSSIBLE
+
+    //2eme étape - Fonction Déplacement de case
+    //
+    //                      Si la case d'en BAS est VIDE -> go
+    //                Sinon si la case de DROITE est VIDE -> go
+    //                Sinon si la case d'en HAUT est VIDE -> go
+    //                Sinon si la case de GAUCHE est VIDE -> go
+    //                          Marquer la case avec la nouvelle valeur de exploration
+    //                  SINON on retourne sur les positions CHECKPOINT de Y et X afin de prendre l'autre chemin inéxploré
+
+
+    console.table(labyrinthe)
+
+}
+
+//______________________ FONCTIONS ____________________
+
+function Scan(Y,X){
+    //Si la case Y n'est pas tout en bas du tableau ET que la case du BAS est VIDE et pas Exploré        on peut vérifier plus bas  ( +1 dans POSSIBLE )
+    if (labyrinthe[Y][X] != labyrinthe.length-1 && labyrinthe[Y+1][X] == o) {
+        possibility++
+    }
+    //Si la case X n'est pas la derniere du tableau ET que la case de DROITE est VIDE et pas Exploré     on peut vérifier à droite  ( +1 dans POSSIBLE )
+    if (labyrinthe[Y][X] != labyrinthe.length-1 && labyrinthe[Y][X+1] == o){
+        possibility++
+    }
+    //Si la case Y n'est pas égale à 0 ET que la case du HAUT est VIDE et pas Exploré                    on peut vérifier en haut   ( +1 dans POSSIBLE )
+    if (labyrinthe[Y][X] != 0 && labyrinthe[Y-1][X] == o){
+        possibility++
+    }
+    //Si la case X n'est pas égale à 0 ET que la case à GAUCHE est VIDE et pas Exploré                   on peut vérifier à gauche  ( +1 dans POSSIBLE )
+    if (labyrinthe[Y][X] != 0 && labyrinthe[Y][X-1] == o){
+        possibility++
+    }
+    // Si POSSIBLE est plus grand ou égale à 2 alors on stock les positions Y et X dans un nouveau tableau [CHECKPOINT] et on réinitialise POSSIBLE
+    if (possibility >= 2){
+        checkpoint.push([Y,X])
+        possibility = 0
+    }
+}
+
+function Deplacement(Y,X){
+    //Si la case d'en BAS est VIDE -> go
+    if (labyrinthe[Y+1][X] == o){
+        labyrinthe[Y+1][X] = exploration++
+    }
+    //Sinon si la case de DROITE est VIDE -> go
+    else if (labyrinthe[Y][X+1] == o){
+        labyrinthe[Y][X+1] = exploration++
+    }
+    //Sinon si la case d'en HAUT est VIDE -> go
+    else if (labyrinthe[Y-1][X] == o){
+        labyrinthe[Y-1][X] = exploration++
+    }
+    //Sinon si la case de GAUCHE est VIDE -> go
+    else if (labyrinthe[Y][X-1] == o){
+        labyrinthe[Y][X-1] = exploration++
+        //SINON on retourne sur les positions CHECKPOINT de Y et X afin de prendre l'autre chemin inéxploré
+    } else {
+        //let checkpointLength = checkpoint.length
+        Y = checkpoint[checkpoint.length-1][0]
+        X = checkpoint[checkpoint.length-1][1]
+
+    }
+}
--- labyrinthe.js
+++ labyrinthe.js
@@ -1,44 +1,50 @@
 //______________________ VARIABLES ____________________
 
-let START = 'S';
-let END = 'G';
+let START = 'START';
+let END = 'GOAL';
 let wall = 'wall';
 let player = "player";
-let emptyCase = "empty";
+let o = "empty";
 let exploration = 0;
 let bottom = 0;
 let right = 0;
 let up = 0;
 let left = 0;
-let checkpoint = [],
+let checkpoint = [];
+let Y = 0;
+let X = 0;
+
 
 //______________________ LABYRINTHE ____________________
 
 labyrinthe = [
     //       0         1        2         3         4         5         6
-        [  START,    wall,   'empty',  'empty',  'empty',  'empty',  'empty'],  // #0
-        [ 'empty',   wall,   'empty',   wall,     wall,    'empty',   wall],   // #1
-        [ 'empty',   wall,   'empty',   wall,     END,     'empty',  'empty'],  // #2
-        [ 'empty',  'empty', 'empty',  'empty',   wall,     wall,    'empty'],  // #3
-        [ 'empty',   wall,   'empty',   wall,    'empty',  'empty',  'empty'],  // #4
-        [ 'empty',   wall,   'empty',  'empty',  'empty',   wall,    'empty']   // #5
+        [  START,    wall,      o,        o,        o,        o,        o    ],     // #0
+        [    o,      wall,      o,       wall,     wall,      o,       wall  ],     // #1
+        [    o,      wall,      o,       wall,     END,       o,        o    ],     // #2
+        [    o,       o,        o,        o,       wall,     wall,      o    ],     // #3
+        [    o,      wall,      o,       wall,      o,        o,        o    ],     // #4
+        [    o,      wall,      o,        o,        o,       wall,      o    ]      // #5
 ]
 
-labyrinthe[0][0]=START
-PlayerEnterCase(1,0)
-PlayerEnterCase(2,0)
-PlayerEnterCase(3,0)
+
+while (labyrinthe[Y][X] != END){
 
 
-console.table(labyrinthe)
+        ScanCase(Y,X)
+
+        DirectionCase(Y,X)
+        console.table(labyrinthe)
+
+}
 
 
 
 //______________________ FONCTIONS ____________________
 
 function PlayerEnterCase(positionY,positionX){
-        if(labyrinthe[positionY][positionX] === player) {
-                labyrinthe[positionY][positionX] = emptyCase
+        if(labyrinthe[positionY][positionX] == player) {
+                labyrinthe[positionY][positionX] = o
         } else {
                 exploration ++
                 labyrinthe[positionY][positionX] = exploration
@@ -47,22 +53,31 @@ function PlayerEnterCase(positionY,positionX){
 }
 
 function ScanCase (positionY,positionX){
+        bottom = 0
+        right = 0
+        up = 0
+        left = 0
 
-        if (labyrinthe[positionY+1][positionX] == emptyCase){
+        if (labyrinthe[positionY+1][positionX] == o){
                 bottom = 1
                 exploration++
+                //labyrinthe[positionY+1][positionX] = labyrinthe[positionY][positionX]
+                Y++
         }
-        if (labyrinthe[positionY][positionX+1] == emptyCase){
+        if (labyrinthe[positionY][positionX+1] == o){
                 right = 1
                 exploration++
+                X++
         }
-        if (labyrinthe[positionY-1][positionX] == emptyCase){
+        if (labyrinthe[positionY != 0][positionX] && labyrinthe[positionY-1][positionX] == o){
                 up = 1
                 exploration++
+                Y--
         }
-        if (labyrinthe[positionY][positionX-1] == emptyCase){
-                bottom = 1
+        if (labyrinthe[positionY][positionX-1] == o){
+                left = 1
                 exploration++
+                X--
         }
         if (exploration >= 2){
                 checkpoint.push(positionY,positionX)
@@ -72,21 +87,25 @@ function ScanCase (positionY,positionX){
 
 function DirectionCase(positionY,positionX){
         if (bottom == 1 && positionY != labyrinthe.length-1){
-                labyrinthe[positionY+1,positionX]
+                labyrinthe[positionY][positionX] = exploration
         }
         else if (right == 1 && positionX !=labyrinthe.length-1){
-                labyrinthe[positionY,positionX+1]
+                labyrinthe[positionY][positionX] = labyrinthe[positionY][positionX+1]
+                labyrinthe[positionY][positionX] = exploration
         }
         else if (up == 1 && positionY != 0){
-                labyrinthe[positionY-1,positionX]
+                labyrinthe[positionY][positionX] = labyrinthe[positionY-1][positionX]
+                labyrinthe[positionY][positionX] = exploration
         }
         else if (left == 1 && positionX !=0){
-                labyrinthe[positionY,positionX-1]
+                labyrinthe[positionY][positionX] = labyrinthe[positionY][positionX-1]
+                labyrinthe[positionY][positionX] = exploration
         }else{
-                //revenir à la position checkpoint
+                labyrinthe[positionY][positionX] = checkpoint[positionY][positionX]
         }
 
 }
+
 
 
 
