f979faecc4da1f7fc8c50c0cc192cd15e6943942
Greg <maza.gregory46@gmail.com>
2021-07-07 16:25:05 +0200

    feat new test step by step

--- LABY2_test.js
+++ LABY2_test.js
@@ -9,6 +9,7 @@ let checkpoint = [];
 let Y = 0;
 let X = 0;
 let possibility = 0
+let way = 'X'
 
 //______________________ LABYRINTHE ____________________
 
@@ -23,48 +24,58 @@ labyrinthe = [
     [    o,      wall,      o,        o,        o,       wall,      o    ]      // #5
 ]
 
+//--------------------------------START------------------------------------------
+
 while (labyrinthe[Y][X] != END){
+    console.table(labyrinthe)
     Scan(Y,X)
     exploration +=1
     //Si la case SUD est VIDE -> go
     if (Y != labyrinthe.length-1 && labyrinthe[Y+1][X] == o){
+        checkpoint.push
         Y = Y +1
         labyrinthe[Y][X] = exploration
+        labyrinthe[Y-1][X] = way
 
     }
     //Sinon si la case EST est VIDE -> go
     else if (X != labyrinthe.length && labyrinthe[Y][X+1] == o){
+        checkpoint.push
         X = X +1
         labyrinthe[Y][X] = exploration
+        labyrinthe[Y][X-1]= way
 
     }
     //Sinon si la case NORD est VIDE -> go
     else if (Y != 0 && labyrinthe[Y-1][X] == o){
+        checkpoint.push
         Y = Y -1
         labyrinthe[Y][X] = exploration
+        labyrinthe[Y+1][X]=way
 
     }
     //Sinon si la case de OUEST est VIDE -> go
     else if (X != 0 && (labyrinthe[Y][X-1] == o || labyrinthe[Y][X-1] == END)) {
+        checkpoint.push
         X = X - 1
-        if (labyrinthe[Y][X] == END){
-           // console.log("YOU WIN !")
-        }else{
-        labyrinthe[Y][X] = exploration
+        if (labyrinthe[Y][X] != END) {
+            labyrinthe[Y][X] = way
         }
+
 
         //SINON on retourne sur les positions CHECKPOINT de Y et X afin de prendre l'autre chemin inéxploré
     } else {
         //let checkpointLength = checkpoint.length
         Y = checkpoint[checkpoint.length-1][0]
         X = checkpoint[checkpoint.length-1][1]
-        checkpoint.pop
+        labyrinthe[Y][X] = exploration
+        //checkpoint.pop()
 
     }
-    console.table(labyrinthe)
     console.log(Y,X)
 }
-console.log('WIN')
+console.log(' ! ! ! ! ! ! ! ! ! ! ! ! CONGRATULATION ! ! ! ! ! ! ! ! ! ! ! ! ! !')
+console.log(' ! ! ! ! ! ! EXIT ON Y = ' + Y + ' AND X = '+ X + ' ! ! ! ! ! !')
 
 //______________________ FONCTIONS ____________________
 
@@ -93,56 +104,3 @@ function Scan(Y,X){
     }
     possibility = 0
 }
-
-function Deplacement(Y,X){
-    //Si la case SUD est VIDE -> go
-    if (Y != labyrinthe.length-1 && labyrinthe[Y+1][X] == o){
-        Y = Y +1
-        labyrinthe[Y][X] = exploration +1
-        return Y
-    }
-    //Sinon si la case EST est VIDE -> go
-    else if (X != labyrinthe.length-1 && labyrinthe[Y][X+1] == o){
-        X = X +1
-        labyrinthe[Y][X] = exploration +1
-        return X
-    }
-    //Sinon si la case NORD est VIDE -> go
-    else if (Y != 0 && labyrinthe[Y-1][X] == o){
-        Y = Y -1
-        labyrinthe[Y][X] = exploration +1
-        return Y
-    }
-    //Sinon si la case de OUEST est VIDE -> go
-    else if (X != 0 && labyrinthe[Y][X-1] == o){
-        X = X - 1
-        labyrinthe[Y][X] = exploration +1
-        return X
-        //SINON on retourne sur les positions CHECKPOINT de Y et X afin de prendre l'autre chemin inéxploré
-    } else {
-        //let checkpointLength = checkpoint.length
-        Y = checkpoint[checkpoint.length-1][0]
-        X = checkpoint[checkpoint.length-1][1]
-        exploration -= 1
-
-    }
-}
-
-// ----------------------------------------------  BROUILLON ---------------------------------------------------
-
-//1ere étape - Fonction Scan pour savoir quelle case nous entoure
-//                      Si la case Y n'est pas tout en bas du tableau ET que la case du BAS est VIDE et pas Exploré        on peut vérifier plus bas  ( +1 dans POSSIBLE )
-//                      Si la case X n'est pas la derniere du tableau ET que la case de DROITE est VIDE et pas Exploré     on peut vérifier à droite  ( +1 dans POSSIBLE )
-//                      Si la case Y n'est pas égale à 0 ET que la case du HAUT est VIDE et pas Exploré                    on peut vérifier en haut   ( +1 dans POSSIBLE )
-//                      Si la case X n'est pas égale à 0 ET que la case à GAUCHE est VIDE et pas Exploré                   on peut vérifier à gauche  ( +1 dans POSSIBLE )
-//
-// Si POSSIBLE est plus grand ou égale à 2 alors on stock les positions Y et X dans un nouveau tableau [CHECKPOINT] et on réinitialise POSSIBLE
-
-//2eme étape - Fonction Déplacement de case
-//
-//                      Si la case d'en BAS est VIDE -> go
-//                Sinon si la case de DROITE est VIDE -> go
-//                Sinon si la case d'en HAUT est VIDE -> go
-//                Sinon si la case de GAUCHE est VIDE -> go
-//                          Marquer la case avec la nouvelle valeur de exploration
-//                  SINON on retourne sur les positions CHECKPOINT de Y et X afin de prendre l'autre chemin inéxploré
